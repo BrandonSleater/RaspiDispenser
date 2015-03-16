fee4ba50f3f9c367945640e808083edb7e46a63d
Brandon Sleater <brandon@cerebrumcorp.com>
2015-03-15 23:59:55 -0700

    Whoops.. conflicting stylesheets

--- resources/views/styles.blade.php
+++ resources/views/styles.blade.php
@@ -1,6 +1,5 @@
 <!-- Styles -->
 <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet'>
-<link href='//cdn.datatables.net/1.10.5/css/jquery.dataTables.min.css' rel='stylesheet'>
 <link href='//cdn.datatables.net/plug-ins/f2c75b7247b/integration/bootstrap/3/dataTables.bootstrap.css' rel='stylesheet'>
 <link href='{{ asset("/css/font-awesome.css") }}' rel='stylesheet'>
 <link href='{{ asset("/css/app.css") }}' rel='stylesheet'>
